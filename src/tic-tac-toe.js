b9c7f34eaa75b222f9fa36b26f0328be48d1ed1a
Maksim <honestliar91@gmail.com>
2021-07-15 16:40:04 +0300

    done

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,109 @@
 class TicTacToe {
     constructor() {
-
+        this.carentPlayer = 'x';
+        this.matrix = [];
+        this.matrix[0] = [];
+        this.matrix[1] = [];
+        this.matrix[2] = [];
+        this.finish = false;
+        this.winner = null;
+        this.noTurn = false;
+        this.counter = 0;
     }
 
     getCurrentPlayerSymbol() {
-
+        this.carentPlayer === 'x' ? 'x' : 'o';
     }
 
     nextTurn(rowIndex, columnIndex) {
+        if (this.carentPlayer === 'x') {
+            this.matrix[rowIndex][columnIndex] = 'x';
+            this.carentPlayer = 'o';
+        } else {
+            this.matrix[rowIndex][columnIndex] = 'o';
+            this.carentPlayer = 'x';
+        }
+        const test = char => {
+            let bool = true;
+            for (let i = 0; i < 3; i++) {
+                bool &= (this.matrix[i][i] === char);
+            }
+            if (bool) {
+                this.winner = char;
+                this.finish = true;
+                return;
+            }
 
+            bool = true;
+            for (let i = 0; i < 3; i++) {
+                bool &= (this.matrix[i][2 - i] === char);
+            }
+            if (bool) {
+                this.winner = char;
+                this.finish = true;
+                return;
+            }
+
+
+            for (let j = 0; j < 3; j++) {
+                bool = true;
+                for (let i = 0; i < 3; i++) {
+                    bool &= (this.matrix[j][i] === char);
+                }
+                if (bool) {
+                    this.winner = char;
+                    this.finish = true;
+                    return;
+                }
+            }
+            
+            for (let j = 0; j < 3; j++) {
+                bool = true;
+                for (let i = 0; i < 3; i++) {
+                    bool &= (this.matrix[i][j] === char);
+                }
+                if (bool) {
+                    this.winner = char;
+                    this.finish = true;
+                    return;
+                }
+            }
+        };
+        this.counter++;
+        test('x');
+        test('o');
+        if (this.counter === 9){
+            this.noTurn = true;
+            this.finish = true;
+        }
     }
 
     isFinished() {
-
+        return this.finish;
     }
 
     getWinner() {
-
+        return this.winner;
     }
 
     noMoreTurns() {
-
+        return this.noTurn;
     }
 
     isDraw() {
-
+        if(this.noTurn && !this.winner){
+           return true;
+        } else {
+            return false;
+        }
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        if (this.matrix[rowIndex][colIndex] === undefined) {
+            return null;
+        } else {
+            return this.matrix[rowIndex][colIndex];
+        }
     }
 }
 
